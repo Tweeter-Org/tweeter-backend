dfa97c70986a325596f9fbda46593f82ebdf8dd7
Alan <ansarialan31@gmail.com>
2023-01-21 21:00:51 +0530

    bugfix no cotroller at route

--- routes/tweetRoutes.js
+++ routes/tweetRoutes.js
@@ -9,7 +9,7 @@ router.get('/bookmark',authverifytoken,tweetController.mysaved);
 router.get('/tagged/:tag',authverifytoken,tweetController.tagtweet);
 router.get('/tags',tweetController.searchtag);
 router.get('/trending',tweetController.trending);
-router.get('/tweet/:tweetId',authverifytoken,)
+router.get('/tweet/:tweetId',authverifytoken,tweetController.gettweet)
 
 router.post('/create',authverifytoken,tweetController.create);
 router.post('/like',authverifytoken,tweetController.liketweet);

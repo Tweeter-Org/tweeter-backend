ab07b52687606a7fade926e91fc3e3b28b020507
alanansari <ansarialan31@gmail.com>
2023-10-10 02:17:32 +0530

    UPDATE: database connection

--- utils/database.js
+++ utils/database.js
@@ -4,20 +4,20 @@ require('dotenv').config();
 
 
 
-// const sequelize = new Sequelize(
-//     process.env.PGDATABASE,
-//     process.env.PGUSER,
-//     process.env.PGPASSWORD,
-//     {
-//         dialect: 'postgres',
-//         host: process.env.PGHOST,
-//         port:process.env.PGPORT
-//     }
-// );
+const sequelize = new Sequelize(
+    process.env.PGDATABASE,
+    process.env.PGUSER,
+    process.env.PGPASSWORD,
+    {
+        dialect: 'postgres',
+        host: process.env.PGHOST,
+        port:process.env.PGPORT
+    }
+);
 
-const sequelize = new Sequelize(process.env.DATABASE_URL,{
-    logging:false
-});
+// const sequelize = new Sequelize(process.env.DATABASE_URL,{
+//     logging:false
+// });
 
 module.exports = {
     sequelize

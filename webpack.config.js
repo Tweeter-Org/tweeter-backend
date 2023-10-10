7b4a2a2aa32eaf66667bac7ddb0506167c4ec456
sugan0tech <sugankpms@gmail.com>
2023-10-10 22:25:57 +0530

    webpack initial setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: './app.js',
+    target: 'node',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                exclude: /node_modules/,
+                use:{
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+    }
+};

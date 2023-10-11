c0b906704969936f4c93e35a2e73de525568fbb8
sugan0tech <sugankpms@gmail.com>
2023-10-11 08:16:05 +0530

    Configured the dev build pipeline

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,12 @@ module.exports = {
                         presets: ['@babel/preset-env']
                     }
                 }
+            },
+            {
+                test: /\.html$/,
+                use:{
+                    loader: "html-loader"
+                }
             }
         ]
     },
